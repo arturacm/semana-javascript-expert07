aed6d2068338b3b1a8fcd387a737d62dcc2e1c32
Artur Moreira <arturacm@gmail.com>
2023-03-04 16:15:23 -0300

    aula04 finalizada

--- classes/class04/pages/titles/src/factories/handGestureFactory.js
+++ classes/class04/pages/titles/src/factories/handGestureFactory.js
@@ -3,6 +3,7 @@ import "https://unpkg.com/@tensorflow/tfjs-backend-webgl@3.7.0/dist/tf-backend-w
 import "https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.4.1646424915/hands.min.js";
 import "https://cdn.jsdelivr.net/npm/@tensorflow-models/hand-pose-detection@2.0.0/dist/hand-pose-detection.min.js";
 import "https://cdn.jsdelivr.net/npm/fingerpose@0.1.0/dist/fingerpose.min.js";
+import "https://cdn.jsdelivr.net/gh/TSedlar/pseudo-styler@1.0.8/pseudostyler.js";
 
 import HandGestureController from "../controllers/handGestureController.js";
 import HandGestureService from "../services/handGestureService.js";
@@ -11,13 +12,14 @@ import Camera from "../../../../lib/shared/camera.js";
 import { fingerLookupIndexes, knownGestures, gestureStrings } from "../utils/index.js";
 
 const camera = await Camera.init();
-
+const styler = new PseudoStyler();
+await styler.loadDocumentStyles();
 fingerLookupIndexes;
 const factory = {
   async initalize() {
     return HandGestureController.initialize({
       camera,
-      view: new HandGestureView({ fingerLookupIndexes }),
+      view: new HandGestureView({ fingerLookupIndexes, styler }),
       service: new HandGestureService({
         knownGestures,
         gestureStrings,
